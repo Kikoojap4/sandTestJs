c03ff6cafb8d21d4e06d1a34c8b50c8d6a40e7d9
Pascal Simon <simon.pascal@etu.u-bordeaux.fr>
2023-10-19 13:41:23 +0200

    gravité + tombé sur la gauche mais sur la droite

--- main.js
+++ main.js
@@ -1,35 +1,43 @@
 let grid = []
-let mainWidth = 0;
-let mainHeight = 0;
+let mainWidth = 0
+let mainHeight = 0
+let prout = 0
+
 
 function setup() {
     
     createCanvas(1440, 798);
     /* faire une boucle pour remplir la variable grid avec des O et aléatoirement mettre un 1*/
-    for(let i = 0; i < 50; i++){
-        grid.push([]);
-        for(let j = 0; j < 25; j++){
-            if(random(0, 1) > 0.5){
-                grid[i].push(1)
-            
-            } else {
-                grid[i].push(0)
-            }
-        }
-    }
-    frameRate(5);
+    makeAGrid();
+    frameRate(60);
 
 
     
 }
-  
+  function makeAGrid(){
+    grid = []
+    for(let i = 0; i < 150; i++){
+        grid.push([]);
+        for(let j = 0; j < 75; j++){
+            if(random(0, 1) > 0.5){
+                grid[i].push(true)
+            
+            } else {
+                grid[i].push(false)
+            }
+        }
+    }
+}
+
   function draw() {
     background(1);
     drawAGrid();
-    for(let i = 0; i < grid.length; i++){
-        for(let j = 0; j < grid[i].length; j++){
-            if(grid[i][j] == 1){
-                gravityGrid(i,j);
+    offsetI = frameCount % 2
+    offsetJ = int(frameCount% 4/2 )
+    for (let i = 0; i < grid.length; i ++) {
+        for (let j = frameCount%2; j < grid[i].length; j +=2) {
+            if (grid[i][j] == true) {
+                gravityGrid(i, j);
             }
         }
     }
@@ -37,10 +45,41 @@ function setup() {
 
 
   function gravityGrid(x,y){
-        if(grid[x][y+1]==0){
-            grid[x][y+1]=1;
-            grid[x][y]=0;
-        }
+    if (x == grid.length - 1 || y == grid[x].length - 1) {
+        return;
+    }
+    tab = [grid[x][y], grid[x + 1][y], grid[x][y + 1], grid[x + 1][y + 1]];
+    if (!tab[0] && tab[1] && !tab[2] && !tab[3]) {
+        grid[x+1][y] = false
+        grid[x+1][y+1] = true
+    } else if (tab[0] && !tab[1] && !tab[2] && !tab[3]) {
+        grid[x][y] = false
+        grid[x][y+1] = true
+    } else if (!tab[0] && tab[1] && !tab[2] && tab[3]) {
+        grid[x+1][y] = false
+        grid[x][y+1] = true
+    } else if (tab[0] && !tab[1] && tab[2] && !tab[3]) {
+        grid[x][y] = false
+        grid[x+1][y+1] = true
+    } else if (!tab[0] && tab[1] && tab[2] && !tab[3]) {
+        grid[x+1][y] = false
+        grid[x+1][y+1] = true
+    } else if (tab[0] && !tab[1] && !tab[2] && tab[3]) {
+        grid[x][y] = false
+        grid[x][y+1] = true
+    } else if (tab[0] && tab[1] && !tab[2] && !tab[3]) {
+        grid[x][y] = false
+        grid[x+1][y] = false
+        grid[x][y+1] = true
+        grid[x+1][y+1] = true
+    } else if (tab[0] && tab[1] && !tab[2] && tab[3]) {
+        grid[x][y] = false
+        grid[x][y+1] = true
+    } else if (tab[0] && tab[1] && tab[2] && !tab[3]) {
+        grid[x+1][y] = false
+        grid[x+1][y+1] = true
+    }
+
   }
 
 
@@ -51,7 +90,7 @@ function setup() {
 
     for(let i = 0; i < grid.length; i++){
         for(let j = 0; j < grid[i].length; j++){
-            if(grid[i][j] == 1){
+            if(grid[i][j] == true){
                 fill(255);
             } else {
                 fill(0);
@@ -65,9 +104,23 @@ function setup() {
         /* si on clique sur une case, on change la valeur de la case */
         let x = floor(mouseX / mainWidth);
         let y = floor(mouseY / mainHeight);
-        if(grid[x][y] == 1){
-            grid[x][y] = 0;
+        if(grid[x][y] == true){
+            grid[x][y] = false;
         } else {
-            grid[x][y] = 1;
+            grid[x][y] = true;
+        }
+    }
+
+    function refresh(){
+        makeAGrid();
+
+    }
+
+
+    function eraseGrid(){
+        for(let i = 0; i < grid.length; i++){
+            for(let j = 0; j < grid[i].length; j++){
+                grid[i][j] = false;
+            }
         }
     }
