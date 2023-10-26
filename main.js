6fd2e3aaebcd989e71dbaddf3680ae5c2829b187
sipascal <simon.pascal.40600@gmail.com>
2023-10-26 18:00:12 +0200

    coordonnés de la souris

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 let grid = []
 let mainWidth = 0
 let mainHeight = 0
-let prout = 0
+let x = 0
+let y = 0
 
 
 function setup() {
@@ -9,18 +10,18 @@ function setup() {
     createCanvas(1440, 798);
     /* faire une boucle pour remplir la variable grid avec des O et aléatoirement mettre un 1*/
     makeAGrid();
-    frameRate(60);
+    frameRate(120);
 
 
     
 }
   function makeAGrid(){
     grid = []
-    for(let i = 0; i < 150; i++){
+    for(let i = 0; i < 100; i++){
         grid.push([]);
-        for(let j = 0; j < 75; j++){
+        for(let j = 0; j < 50; j++){
             if(random(0, 1) > 0.5){
-                grid[i].push(true)
+                grid[i].push(false)
             
             } else {
                 grid[i].push(false)
@@ -30,57 +31,63 @@ function setup() {
 }
 
   function draw() {
+    x = floor(mouseX / mainWidth);
+    y = floor(mouseY / mainHeight);
     background(1);
     drawAGrid();
-    offsetI = frameCount % 2
-    offsetJ = int(frameCount% 4/2 )
-    for (let i = 0; i < grid.length; i ++) {
-        for (let j = frameCount%2; j < grid[i].length; j +=2) {
-            if (grid[i][j] == true) {
-                gravityGrid(i, j);
-            }
-        }
-    }
+
+    fill(255,0,0)
+    text(x+' '+y, 10, 10)
+
+    // offsetI = frameCount % 2
+    // offsetJ = int(frameCount% 4/2 )
+    // for (let i = 0; i < grid.length; i ++) {
+    //     for (let j = frameCount%2; j < grid[i].length; j +=2) {
+    //         if (grid[i][j] == true) {
+    //             gravityGrid(i, j);
+    //         }
+    //     }
+    // }
   } 
 
 
-  function gravityGrid(x,y){
-    if (x == grid.length - 1 || y == grid[x].length - 1) {
-        return;
-    }
-    tab = [grid[x][y], grid[x + 1][y], grid[x][y + 1], grid[x + 1][y + 1]];
-    if (!tab[0] && tab[1] && !tab[2] && !tab[3]) {
-        grid[x+1][y] = false
-        grid[x+1][y+1] = true
-    } else if (tab[0] && !tab[1] && !tab[2] && !tab[3]) {
-        grid[x][y] = false
-        grid[x][y+1] = true
-    } else if (!tab[0] && tab[1] && !tab[2] && tab[3]) {
-        grid[x+1][y] = false
-        grid[x][y+1] = true
-    } else if (tab[0] && !tab[1] && tab[2] && !tab[3]) {
-        grid[x][y] = false
-        grid[x+1][y+1] = true
-    } else if (!tab[0] && tab[1] && tab[2] && !tab[3]) {
-        grid[x+1][y] = false
-        grid[x+1][y+1] = true
-    } else if (tab[0] && !tab[1] && !tab[2] && tab[3]) {
-        grid[x][y] = false
-        grid[x][y+1] = true
-    } else if (tab[0] && tab[1] && !tab[2] && !tab[3]) {
-        grid[x][y] = false
-        grid[x+1][y] = false
-        grid[x][y+1] = true
-        grid[x+1][y+1] = true
-    } else if (tab[0] && tab[1] && !tab[2] && tab[3]) {
-        grid[x][y] = false
-        grid[x][y+1] = true
-    } else if (tab[0] && tab[1] && tab[2] && !tab[3]) {
-        grid[x+1][y] = false
-        grid[x+1][y+1] = true
-    }
+//   function gravityGrid(x,y){
+//     if (x == grid.length - 1 || y == grid[x].length - 1) {
+//         return;
+//     }
+//     tab = [grid[x][y], grid[x + 1][y], grid[x][y + 1], grid[x + 1][y + 1]];
+//     if (!tab[0] && tab[1] && !tab[2] && !tab[3]) {
+//         grid[x+1][y] = false
+//         grid[x+1][y+1] = true
+//     } else if (tab[0] && !tab[1] && !tab[2] && !tab[3]) {
+//         grid[x][y] = false
+//         grid[x][y+1] = true
+//     } else if (!tab[0] && tab[1] && !tab[2] && tab[3]) {
+//         grid[x+1][y] = false
+//         grid[x][y+1] = true
+//     } else if (tab[0] && !tab[1] && tab[2] && !tab[3]) {
+//         grid[x][y] = false
+//         grid[x+1][y+1] = true
+//     } else if (!tab[0] && tab[1] && tab[2] && !tab[3]) {
+//         grid[x+1][y] = false
+//         grid[x+1][y+1] = true
+//     } else if (tab[0] && !tab[1] && !tab[2] && tab[3]) {
+//         grid[x][y] = false
+//         grid[x][y+1] = true
+//     } else if (tab[0] && tab[1] && !tab[2] && !tab[3]) {
+//         grid[x][y] = false
+//         grid[x+1][y] = false
+//         grid[x][y+1] = true
+//         grid[x+1][y+1] = true
+//     } else if (tab[0] && tab[1] && !tab[2] && tab[3]) {
+//         grid[x][y] = false
+//         grid[x][y+1] = true
+//     } else if (tab[0] && tab[1] && tab[2] && !tab[3]) {
+//         grid[x+1][y] = false
+//         grid[x+1][y+1] = true
+//     }
 
-  }
+//   }
 
 
   function drawAGrid(){
@@ -100,16 +107,21 @@ function setup() {
     }
   }
 
+
+  function round(){
+    
+  }
     function mousePressed(){
         /* si on clique sur une case, on change la valeur de la case */
-        let x = floor(mouseX / mainWidth);
-        let y = floor(mouseY / mainHeight);
+        x = floor(mouseX / mainWidth);
+        y = floor(mouseY / mainHeight);
         if(grid[x][y] == true){
             grid[x][y] = false;
         } else {
             grid[x][y] = true;
         }
     }
+
 
     function refresh(){
         makeAGrid();
